b4c47b9d30cf4258d1d3e3c15101ece03d7d3f2e
niksparab84 <niksparab84@gmail.com>
2025-07-05 15:33:59 +0530

    test class
    test class

---
+++ src/test/java/com/trafigura/positionManagementSystem/PositionManagementSystemApplicationTest.java
@@ -0,0 +1,14 @@
+package com.trafigura.positionManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class PositionManagementSystemApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
